811937b2495fbaa5dcf3e1aeb9d268f3dafab165
MwaksC <cormwandau@gmail.com>
2023-08-10 06:25:57 -0700

    My task

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN.H_
-#define _MAIN.H_
+#ifndef MAIN.H_
+#define MAIN.H_
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);

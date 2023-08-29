0e881610eb6730c6723fa6ad2a6ca1cc4ff1d83d
MwaksC <cormwandau@gmail.com>
2023-08-29 07:47:12 -0700

    Soso

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,34 +10,28 @@
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
-{
-	int a, b;
+	{
+	int n1, n2, result;
+	int (*p)(int, int);
 
-	int (*operation)(int, int);
-
-	if (argc != 4)
+	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (argv[2][1])
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+
+	if (p == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	result = p(n1, n2);
 
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", operation(a, b));
+	printf("%d\n", result);
 	return (0);
 }

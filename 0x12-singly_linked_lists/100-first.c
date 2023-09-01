1edbd741d67c7bc432713e2c079d67e0eef751cd
MwaksC <136140107+MwaksC@users.noreply.github.com>
2023-09-01 15:14:57 +0300

    Delete 0x12-singly_linked_lists directory

--- 0x12-singly_linked_lists/100-first.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void __attribute__((constructor)) hare(void);
-void hare(void)
-{
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
-}
